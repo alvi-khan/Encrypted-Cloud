026bf6db3c5c14063df4719c750fe11f7eea40df
Alvi Khan <55395950+alvi-khan@users.noreply.github.com>
2022-06-12 00:08:51 +0600

    Implemented 'Log Out' functionality. Contributes to #9.

--- lib/views/FileViewer.dart
+++ lib/views/FileViewer.dart
@@ -141,13 +141,32 @@ class _FileViewerState extends State<FileViewer> {
               if (selections == 0)
                 Padding(
                   padding: const EdgeInsets.only(right: 10),
-                  child: CircleAvatar(
-                      backgroundColor: Colors.transparent,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: Image.network(account.user!.photoUrl!),
-                        // TODO handle no profile image case
-                      )
+                  child: PopupMenuButton(
+                    icon: CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: Image.network(account.user!.photoUrl!),
+                          // TODO handle no profile image case
+                        )
+                    ),
+                    color: Colors.blueGrey.shade500,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    splashRadius: null,
+                    itemBuilder: (context) {
+                      PopupMenuItem deleteButton = PopupMenuItem(
+                          onTap: () => account.signOut(),
+                          textStyle: const TextStyle(color: Colors.white),
+                          child: Row(
+                            children: const [
+                              Icon(Icons.logout_rounded),
+                              SizedBox(width: 10),
+                              Text("Log Out"),
+                            ],
+                          )
+                      );
+                      return List.from([deleteButton]);
+                    },
                   ),
                 ),
               if (selections != 0)
