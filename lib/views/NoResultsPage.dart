10868fcbda60c62e0307268f446724b554b91c20
Alvi Khan <55395950+alvi-khan@users.noreply.github.com>
2022-06-17 23:02:48 +0600

    Fixed #13, 'No Results' page missing the upload button.

--- lib/views/NoResultsPage.dart
+++ lib/views/NoResultsPage.dart
@@ -1,3 +1,4 @@
+import 'package:encrypted_cloud/components/UploadButton.dart';
 import 'package:encrypted_cloud/components/appbar/ProfileMenuButton.dart';
 import 'package:flutter/material.dart';
 
@@ -12,21 +13,28 @@ class NoResultsPage extends StatelessWidget {
         actions: const [ProfileMenuButton()],
       ),
       backgroundColor: Colors.blueGrey.shade800,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 100),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Icon(Icons.image_rounded, size: 150, color: Colors.blueGrey.shade100),
-            const SizedBox(height: 20),
-            Text(
-              "Images you upload will show up here.",
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.blueGrey.shade100, fontSize: 20, height: 1.5),
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Icon(Icons.image_rounded, size: 150, color: Colors.blueGrey.shade100),
+                  const SizedBox(height: 20),
+                  Text(
+                    "Images you upload will show up here.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.blueGrey.shade100, fontSize: 20, height: 1.5),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          const UploadButton(),
+        ],
       ),
     );
   }
